ad99407dc2268fb4cf7e95d1fd040ff776821b07
onejulian2014 <onejulian2014@gmail.com>
2022-09-03 15:47:02 -0500

    :sparkles: aws config

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"aws-golang/src/infraestructure"
+)
+
 
 func main() {
-	fmt.Println("Hello world")
+	infraestructure.S3.Region="us-east-1"
+	infraestructure.S3.NewSession(infraestructure.S3.Region)
+	infraestructure.S3.Ls()
+
+	// infraestructure.S3.Upload("subeme.txt","golandia-julian","subido.txt")
+	// infraestructure.S3.GenerateUrl("golandia","subido.txt")
 }
